6909e745ba6872348990091ebe242ef3794d78e2
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-24 16:04:17 +0200

    add JsonParser

---
+++ Assets/Scripts/Utils/JsonParser.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class JsonParser
+    {
+        public static T Parse<T>(string jsonString)
+        {
+            return JsonUtility.FromJson<T>(jsonString);
+        }
+
+        public static T LoadAndParse<T>(string filePath)
+        {
+            var jsonString = File.ReadAllText(filePath);
+            return Parse<T>(jsonString);
+        }
+    }
+}
