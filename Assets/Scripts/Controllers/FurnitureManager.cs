b630ece3f6d0d37bcdc68a1e20ce4ee3c74ec7ee
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-25 10:11:57 +0200

    change furniture material default

--- Assets/Scripts/Controllers/FurnitureManager.cs
+++ Assets/Scripts/Controllers/FurnitureManager.cs
@@ -7,6 +7,7 @@ namespace Controllers
 {
     public class FurnitureManager : MonoBehaviour
     {
+        [SerializeField] private Material _material;
         [SerializeField] private TextAsset[] _furnitureJsonFiles;
         private const float _spacingBetweenFurnitures = 100f;
 
@@ -19,9 +20,21 @@ namespace Controllers
                 var furnitureData = JsonParser.Parse<Furniture>(jsonFile.text);
                 var builder = gameObject.AddComponent<FurnitureBuilder>().SetData(furnitureData);
                 var furniture = builder.Build();
+                
+                ChangeFurnitureMaterial(furniture, _material);
 
                 furniture.transform.position = new Vector3(furnitureData.width / 2 + xOffset, 0, 0);
                 xOffset += furnitureData.width + _spacingBetweenFurnitures;
+            }
+        }
+        
+        private static void ChangeFurnitureMaterial(GameObject furniture, Material material)
+        {
+            var renderers = furniture.GetComponentsInChildren<Renderer>();
+
+            foreach (var furnitureRenderer in renderers)
+            {
+                furnitureRenderer.material = material;
             }
         }
     }
